cd6307ff307ef7921ff651afeb7e4d1f193582ae
Polina <polinaucc@gmail.com>
2020-04-29 23:23:22 +0300

    added AppServlet and EncodingFilter

---
+++ src/main/java/ua/polina/controller/AppServlet.java
@@ -0,0 +1,50 @@
+package ua.polina.controller;
+
+import ua.polina.controller.command.Command;
+import ua.polina.controller.command.admin.AddDescriptionCommand;
+import ua.polina.model.service.DescriptionService;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet("/add-description")
+public class AppServlet extends HttpServlet {
+    private Map<String, Command> commands = new HashMap<>();
+
+    private DescriptionService descriptionService = new DescriptionService();
+
+    public void init(ServletConfig servletConfig){
+        System.out.println("name:"+servletConfig.getInitParameter("name"));
+        commands.put("add-description", new AddDescriptionCommand(descriptionService));
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        processRequest(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        processRequest(request, response);
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String path = request.getRequestURI();
+        path = path.replace("/", "");
+        Command command = commands.getOrDefault(path, (r) -> "/index.jsp");
+        String page = command.execute(request);
+
+        if(page.contains("redirect")){
+            response.sendRedirect(page.replaceAll("redirect:", ""));
+        }
+        else {
+            request.getRequestDispatcher(page).forward(request, response);
+        }
+    }
+}
